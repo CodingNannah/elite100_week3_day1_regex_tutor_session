2b62f897985a4cda0beb29593a68f06155a1a18f
CodingNannah <codingnannah@gmail.com>
2022-09-19 18:05:47 -0600

    Updated Tutor file

--- regex.py
+++ regex.py
@@ -17,15 +17,28 @@ for line in regex_data:
 #     m_name = re.compile(r'(\s[A-Z][a-z]*)?') # middle name = could be there but doesn't have to be! = ?
 #     l_name = re.compile(r'\s[A-Z][a-z]+') 
 #     names = f'{f_name} {m_name} {l_name}'
-#     name = names.findall(line)
+#     name = names.findall(line) # pattern.findall('string')
 #     if name: 
 #         print(name)
 #     else:
 #         print("None")
-        
-#     f_name = re.compile(r'[A-Z][a-z]+')
-#     m_name = re.compile(r'(\s[A-Z][a-z]*)?') # middle name = could be there but doesn't have to be! = ?
-#     l_name = re.compile(r'\s[A-Z][a-z]+') 
 
+"""
+Input from file regex-test.txt
 
-    # pattern.findall('string')
+Expected output:
+Abraham Lincoln
+Andrew P Garfield
+Connor Milliken
+Jordan Alexander Williams
+None
+None
+
+Getting: 
+[('Abraham', '', ' Lincoln')]
+[('Andrew', ' P', ' Garfield')]
+[('Connor', '', ' Milliken')]
+[('Jordan', ' Alexander', ' Williams')]
+None
+None
+""" 
